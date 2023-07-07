e499d26fc77a15523596413e28ee53bfb1958d2e
pieceofpizza <lisanovadarina@gmail.com>
2023-07-07 17:55:27 +0300

    Initial Commit

---
+++ rickAndMortySwiftUI/rickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortySwiftUIApp.swift
+//  rickAndMortySwiftUI
+//
+//  Created by Дарина Лисанова on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
